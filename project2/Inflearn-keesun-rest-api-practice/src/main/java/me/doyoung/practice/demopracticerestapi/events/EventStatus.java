7603a5567c1b4f8694f5428371a3dce851e622ce
Azderica <mh97888@gmail.com>
2020-09-13 12:18:49 +0900

    update project 2

--- project2/Inflearn-keesun-rest-api-practice/src/main/java/me/doyoung/practice/demopracticerestapi/events/EventStatus.java
+++
@@ -1,5 +0,0 @@
-package me.doyoung.practice.demopracticerestapi.events;
-
-public enum EventStatus {
-    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
-}
---
+++ project2/demo-inflean-rest-api/src/main/java/myepark/ebay/demoinfleanrestapi/events/EventValidator.java
@@ -0,0 +1,40 @@
+package myepark.ebay.demoinfleanrestapi.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    // reject() : field 에러에 들어가게 된다.
+    // rejectValue() : global 에러에 들어가게 된다.
+
+    public void validate(EventDto eventDto, Errors errors) {
+        if(eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
+            errors.reject("wrongPrices", "Values for Prices are wrong.");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if(endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong.");
+        }
+
+        LocalDateTime beginEventDateTime = eventDto.getBeginEventDateTime();
+        if(beginEventDateTime.isBefore(eventDto.getEndEventDateTime()) ||
+                beginEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime())) {
+            errors.rejectValue("beginEventDateTime", "wrongValue", "beginEventDateTime is wrong.");
+        }
+
+        LocalDateTime closeEnrollmentDateTime = eventDto.getCloseEnrollmentDateTime();
+        if(closeEnrollmentDateTime.isBefore(eventDto.getBeginEnrollmentDateTime()) ||
+                closeEnrollmentDateTime.isBefore(eventDto.getBeginEventDateTime())) {
+            errors.rejectValue("closeEnrollmentDateTime", "wrongValue", "closeEnrollmentDateTime is wrong.");
+        }
+
+    }
+}
+
