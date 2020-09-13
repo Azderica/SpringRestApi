2a83984cc2e52f1a34d6128bd32c3e3cf65e17ec
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2020-09-13 11:27:54 +0900

    Add files via upload

---
+++ project2/Inflearn-keesun-rest-api-practice/src/main/java/me/doyoung/practice/demopracticerestapi/events/EventController.java
@@ -0,0 +1,53 @@
+package me.doyoung.practice.demopracticerestapi.events;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.http.ResponseEntity.badRequest;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
+
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator) {
+        this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
+    }
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+
+        // 유효하지 않은 값이 request로 들어오면 302 에러 발생시키기
+        // Errors 객체는 자바 빈 표준을 준수하지 않기 때문에 Response에 담을 수 없다.
+        if(errors.hasErrors()) {
+            return badRequest().body(errors);
+        }
+
+        eventValidator.validate(eventDto, errors);
+
+        if(errors.hasErrors()) {
+            return badRequest().body(errors);
+        }
+
+        Event event = modelMapper.map(eventDto, Event.class);
+        event.update();
+        Event newEvent = this.eventRepository.save(event);
+        URI createdUri = linkTo((EventController.class)).slash(newEvent.getId()).toUri();
+        return ResponseEntity.created(createdUri).body(event);
+    }
+}
